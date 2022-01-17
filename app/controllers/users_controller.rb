63d50cb689f1e3f02e5ff73ddfb5455223aa3571
Muchiripatrick1 <muchiripatrick86@gmail.com>
2022-01-17 21:03:30 +0300

    corrections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,23 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
     def new
       @user = User.new
     end
    
     def show
-        @user = User.find(params[:id])
-      end
-      def create
+    end
+    def create
         @user = User.new(user_params)
         if @user.save
           redirect_to user_path(@user.id)
         else
           render :new
-        end
       end
+    end
       def edit
-        @user = User.find(params[:id]
-        end
+      end
+
       def update
-        @user = User.find(params[:id])
           if @user.update(user_params)
               redirect_to user_path(@user.id)
           else
@@ -26,6 +25,10 @@ class UsersController < ApplicationController
           end
       end
      private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   get 'sessions/new'
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show, :edit, :update]
   resources :feeds do
     collection do
       post :confirm
-      resources :sessions, only: [:new, :create, :destroy]
-      resources :users, only: [:new, :create, :show, :edit, :update]
-
     end
   end
 end
